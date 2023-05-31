fix normalization & separate fit, add true AOI

model.py
@@ -24,7 +24,7 @@ from partition import random_partition
 
 # function to add lons, lats, distances from nadir, and other
 # variables to the dataframe of polygons and counts.
-def get_data(gdf, fov_center=None, mapping=None, ecliptic=False):
+def get_data(gdf, fov_center=None, mapping=None, shower_data=None):
 
     # get the centroids of the polygons and their latitudes and longitudes
     centroids = [list(poly.centroid.xy) for poly in gdf.geometry]
@@ -38,7 +38,26 @@ def get_data(gdf, fov_center=None, mapping=None, ecliptic=False):
     # obtain a list of coordinates to compute the distances from the center of the field of view
     coords = list(zip(lats, lons))
     if fov_center is not None:
-        fov_dists = np.array(haversine_vector([fov_center]*len(coords), coords))
+        def unit(vec):
+            return vec/np.linalg.norm(vec)
+        transformer = pyproj.Transformer.from_crs(
+            {"proj":'latlong', "ellps":'WGS84', "datum":'WGS84'},
+            {"proj":'geocent', "ellps":'WGS84', "datum":'WGS84'}
+        )
+
+        nadir_lat = fov_center[0]
+        nadir_lon = fov_center[1]
+
+        goes_alt = 35786 * 1000
+        sat_vec = np.array(transformer.transform(nadir_lon,nadir_lat,goes_alt,radians = False))
+
+        lats = bdf.latitude
+        lons = bdf.longitude
+
+        loc_vecs = np.array([transformer.transform(lon,lat,10*1000,radians = False) for lat,lon in zip(coords)])
+
+        look_vecs = sat_vec-loc_vecs
+        aois = [np.degrees(np.arccos(np.dot(unit(sat_vec),unit(look_vec)))) for look_vec in look_vecs]
 
     # get the pixel pitches of the pixels on the CCD corresponding to the centroids
     if mapping is not None:
@@ -49,7 +68,7 @@ def get_data(gdf, fov_center=None, mapping=None, ecliptic=False):
 
     from netCDF4 import Dataset
     flash_data = Dataset('data/LISOTD_HRFC_V2.3.2015.nc')
-    flash_dens = np.sqrt(np.array([get_flash_density(flash_data, lat, lon) for lat, lon in zip(lats, lons)]))
+    flash_dens = np.array([get_flash_density(flash_data, lat, lon) for lat, lon in zip(lats, lons)])
     flash_dens = np.nan_to_num(flash_dens, copy=True, nan=np.nanmean(flash_dens))
     assert sum(np.isnan(flash_dens))==0
 
@@ -59,17 +78,18 @@ def get_data(gdf, fov_center=None, mapping=None, ecliptic=False):
 
     if shower_data is not None:
         for s, filename in shower_data.items():
-            print(f'loading {filename}')
-            shower_rate = np.array(pd.read_csv(filename))
-            shower_lats = shower_rate['lat']
-            rates = shower_rate['dens']
+            if s in gdf.columns:
+                print(f'loading {filename}')
+                shower_rate = pd.read_csv(filename)
+                shower_lats = np.array(shower_rate['lat'])
+                rates = np.array(shower_rate['dens'])
 
-            gdf[s+'rate'] = gdf[s]*np.interp(lats, shower_lats, rates)
+                gdf[s+'rate'] = gdf[s]*np.interp(lats, shower_lats, rates)
 
     # add new columns to the dataframe
     gdf['lat'] = lats
     gdf['lon'] = lons
-    gdf['fov_dist'] = fov_dists
+    gdf['aoi'] = aois
     gdf['flash_dens'] = flash_dens
     gdf['land'] = land
 
@@ -81,7 +101,7 @@ def get_data(gdf, fov_center=None, mapping=None, ecliptic=False):
 def fit(data, f_lat, f_fov, biases, showers, shower_known=False, **kwargs):
     # extract variables from the data
     lat = np.array(data['lat'])
-    fov = np.array(data['fov_dist'])
+    fov = np.array(data['aoi'])
     area = np.array(data['area'])
     max_area = max(area)
     area = area/max_area
@@ -141,20 +161,19 @@ def fit(data, f_lat, f_fov, biases, showers, shower_known=False, **kwargs):
 
         n1 = len(biases)+len(f_fov.split('+'))
         bias_term = np.exp(tt.dot(X.values[:,:n1], beta[:n1]))
-        total_rate = 0
-        ni = n1
-        step = len(f_fov.split('+'))
 
         if shower_known:
             print('shower rates are known')
-            intercept = pm.Cauchy(f"{s}intercept", alpha=0, beta=0.5)
+            intercept = pm.Cauchy(f"intercept", alpha=0, beta=0.5)
             shower_rate = 0
             for s in showers:
-                s_intercept = pm.HalfCauchy(f"{s}intercept", alpha=0, beta=0.5)
+                s_intercept = pm.HalfCauchy(f"{s}intercept", beta=0.5)
                 shower_rate += s_intercept*np.array(data[f'{s}rate'])
-            total_rate = np.exp(intercept + tt.dot(X.values, beta)) + shower_rate
+            total_rate = np.exp(intercept + tt.dot(X.values[:,n1:], beta[n1:])) + shower_rate
         else:
             total_rate = 0
+            ni = n1
+            step = len(f_fov.split('+'))
             for i in range(len(showers)+1):
                 s = (['']+showers)[i]
                 intercept = pm.Cauchy(f"{s}intercept", alpha=0, beta=0.5)
@@ -172,10 +191,10 @@ def fit(data, f_lat, f_fov, biases, showers, shower_known=False, **kwargs):
         # sample
         import pymc.sampling_jax
         # MCMC
-        draws = 5000
-        chains = 2
-        idata = pm.sampling_jax.sample_numpyro_nuts(draws, tune=2000, chains=chains,
-                                                    idata_kwargs={'log_likelihood':True})
+        draws = 10000
+        chains = 4
+        idata = pm.sampling_jax.sample_numpyro_nuts(draws, tune=5000, chains=chains,
+                                                    idata_kwargs={'log_likelihood':False})
         # don't include y in prior as extreme values of the coefficients make the rate of the
         # Poisson explode
         # idata.extend(pm.sample_prior_predictive(10000))
@@ -335,7 +354,7 @@ def full_model(g16=None, g17=None, separate=False, n_points=1000,
         fov = [goes_e_fov, goes_w_fov][num]
         fov_center = [goes_e, goes_w][num]
         gdf = discretize(bdf, fov=fov, lon=fov_center[1], n_points=n_points, showers=showers)
-        data = get_data(gdf, fov_center=fov_center, ecliptic=ecliptic, shower_data=shower_data)
+        data = get_data(gdf, fov_center=fov_center, shower_data=shower_data)
         data['sat'] = sats[num]
         datas.append(data)
     #    with open('models/data.pkl','wb') as f:
@@ -353,7 +372,8 @@ def full_model(g16=None, g17=None, separate=False, n_points=1000,
     else:
         results = []
         for data in datas:
-            result = fit(data, f_lat, f_fov, biases)
+            result = fit(data, f_lat, f_fov, biases, showers, shower_known)
             results.append(result)
+        datas = [d.to_crs(datas[0].crs) for d in datas]
         data = GeoDataFrame(pd.concat(datas, ignore_index=True), crs=datas[0].crs)
-        return dict(results=results, data=data)
+        return dict(results=results, data=data, f_lat=f_lat, f_fov=f_fov, biases=biases)

plotting.py
@@ -44,7 +44,7 @@ def plot_polygons(gdf, data, filename=None, label='Polygon area (km$^{-2}$)',
     for num, poly in enumerate(gdf.geometry):
         ax.add_geometries([poly], crs=crs, color=colors[num], linewidth=0)
 
-    points = [Point(lon, lat) for lon, lat in zip(gdf['lon'], gdf['lat']*90)]
+    points = [Point(lon, lat) for lon, lat in zip(gdf['lon'], gdf['lat'])]
     points = GeoDataFrame(geometry=points, crs='epsg:4326').to_crs(crs.proj4_init).geometry
     x = np.array([p.x for p in points])
     y = np.array([p.y for p in points])
@@ -66,7 +66,7 @@ def plot_polygons(gdf, data, filename=None, label='Polygon area (km$^{-2}$)',
         ax2.stock_img()
         for num, poly in enumerate(gdf.geometry):
             ax2.add_geometries([poly], crs=crs, color=colors[num], linewidth=0)
-        points = [Point(lon, lat) for lon, lat in zip(gdf['lon'], gdf['lat']*90)]
+        points = [Point(lon, lat) for lon, lat in zip(gdf['lon'], gdf['lat'])]
         points = GeoDataFrame(geometry=points, crs='epsg:4326').to_crs(crs2.proj4_init).geometry
         x = np.array([p.x for p in points])
         y = np.array([p.y for p in points])
@@ -88,9 +88,9 @@ def plot_polygons(gdf, data, filename=None, label='Polygon area (km$^{-2}$)',
     # plt.show()
 
 
-def reformat_result(result):
+def reformat_result(result, which=0):
     import copy
-    r = result['results'][0]
+    r = result['results'][which]
     pos = r['idata'].posterior
     pos2 = {}
     for key in pos.keys():
@@ -107,7 +107,7 @@ def reformat_result(result):
     pos2['chain'] = pos.chain
     pos2['draw'] = pos.draw
     result2 = copy.deepcopy(result)
-    result2['results'][0]['idata'] = {'posterior':pos2}
+    result2['results'][which]['idata'] = {'posterior':pos2}
     return result2
 
 
@@ -131,9 +131,9 @@ def get_varfactor(f, var, x, shower='', adjust=None, m_ap=None, pos=None, chain=
     return varfactor, zero
 
 
-def plot_lat_result(result, title, filename=None, plot_map=True, max_lat=55, normalize=True, symmetric=False, shower='', theory=None, show=False):
+def plot_lat_result(result, title, which=0, filename=None, plot_map=True, max_lat=55, normalize=True, symmetric=False, shower='', theory=None, show=False, legend_col=2):
 
-    result = reformat_result(result)
+    result = reformat_result(result, which=which)
 
     plt.style.use('default')
     plt.rcParams['text.usetex'] = True
@@ -146,13 +146,13 @@ def plot_lat_result(result, title, filename=None, plot_map=True, max_lat=55, nor
 
     f_lat = result['f_lat']
     #result = result['results'][0]
-    pos = result['results'][0]['idata']['posterior']
+    pos = result['results'][which]['idata']['posterior']
     #m_ap = result['map']
-    max_area = result['results'][0]['max_area']
+    max_area = result['results'][which]['max_area']
 
     top_y = 0
     y_plots = np.zeros((400, 200//(int(symmetric)+1)))
-    x_plot = np.linspace(-max_lat, max_lat, 200)/90
+    x_plot = np.linspace(-max_lat, max_lat, 200)
     for i in range(401):
         chain = random.randint(0, len(pos['chain'])-1)
         draw = random.randint(0, len(pos['draw'])-1)
@@ -163,7 +163,7 @@ def plot_lat_result(result, title, filename=None, plot_map=True, max_lat=55, nor
             linewidth = 0.5
 
             intercept = pos[f"{shower}intercept"][chain][draw].data
-            adjust = result['results'][0]['adjust']
+            adjust = result['results'][which]['adjust']
             varfactor, zero = get_varfactor(f_lat, 'lat', x_plot, shower, adjust, pos=pos, chain=chain, draw=draw)
             #if shower != '':
             #    intercept += pos[shower][chain][draw].data
@@ -190,15 +190,15 @@ def plot_lat_result(result, title, filename=None, plot_map=True, max_lat=55, nor
         if i != 400:
             y_plots[i, :] = y_plot
         if not (i==400 and not plot_map):
-            ax1.plot(x_plot[midpoint:]*90, y_plot, color=color, alpha=alpha, linewidth=linewidth)
+            ax1.plot(x_plot[midpoint:], y_plot, color=color, alpha=alpha, linewidth=linewidth)
 
     # plot quantiles
     top_quantile = np.quantile(y_plots, 0.90, axis=0)
     bottom_quantile = np.quantile(y_plots, 0.10, axis=0)
     median = np.quantile(y_plots, 0.5, axis=0)
-    plt.plot(x_plot[midpoint:]*90, top_quantile, color='red', linewidth=1, linestyle='--')
-    plt.plot(x_plot[midpoint:]*90, bottom_quantile, color='red', linewidth=1, linestyle='--')
-    plt.plot(x_plot[midpoint:]*90, median, color='red', linewidth=1, linestyle='-')
+    plt.plot(x_plot[midpoint:], top_quantile, color='red', linewidth=1, linestyle='--')
+    plt.plot(x_plot[midpoint:], bottom_quantile, color='red', linewidth=1, linestyle='--')
+    plt.plot(x_plot[midpoint:], median, color='red', linewidth=1, linestyle='-')
 
     plt.xlabel('Latitude (°)')
     plt.ylabel('Normalized bolide flux')
@@ -242,7 +242,7 @@ def plot_lat_result(result, title, filename=None, plot_map=True, max_lat=55, nor
     else:
         handles.extend([line, line2, line3])
 
-    plt.legend(handles=handles, frameon=False, ncol=2)
+    plt.legend(handles=handles, frameon=False, ncol=legend_col)
     plt.gcf().set_size_inches((4, 3))
     # plt.savefig(f'plots/{filename}.png', dpi=300, bbox_inches='tight')
     # plt.savefig(f'plots/{filename}.pgf', bbox_inches='tight')
@@ -272,7 +272,7 @@ def plot_fov_result(result, title, filename=None, plot_map=True, normalize=False
     top_y = 0
     y_plots = np.zeros((400, 200))
     for i in range(401):
-        x_plot = np.linspace(0, 7300, 200)/10000
+        x_plot = np.linspace(0, 7300, 200)
         chain = random.randint(0, len(pos['chain'])-1)
         draw = random.randint(0, len(pos['draw'])-1)
 
@@ -302,7 +302,7 @@ def plot_fov_result(result, title, filename=None, plot_map=True, normalize=False
         top_y = max(max(y_plot), top_y)
 
         if angle:
-            x_plot = [distance_to_angle(x*10000) for x in x_plot]
+            x_plot = [distance_to_angle(x) for x in x_plot]
 
         if i != 400:
             y_plots[i, :] = y_plot
@@ -332,7 +332,7 @@ def plot_fov_result(result, title, filename=None, plot_map=True, normalize=False
         ax1.set_xlabel('distance from fov nadir (km)')
     ax1.set_ylim(0, top_y)
     plt.xlim(0, max(x_plot))
-    ax1.set_ylabel('Normalized bolide rate')
+    ax1.set_ylabel('Normalized bolide flux')
     plt.title(title)
 
     handles, labels = plt.gca().get_legend_handles_labels()
@@ -351,3 +351,75 @@ def plot_fov_result(result, title, filename=None, plot_map=True, normalize=False
     if show:
         plt.show()
     plt.close()
+
+def plot_fov_results(results, title, angle=True, truth=None, figsize=(4,3)):
+
+    plt.style.use('default')
+    plt.rcParams['text.usetex'] = True
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['xtick.top'] = True
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.rcParams['ytick.right'] = True
+
+    fig, ax1 = plt.subplots()
+    fig.set_size_inches(figsize)
+
+    colors = ['darkblue','brown']
+
+    for n, result in enumerate(results):
+        result = reformat_result(result)
+        f_fov = result['f_lat']
+        pos = result['results'][0]['idata']['posterior']
+        m_ap = result['results'][0]['map']
+        max_area = result['results'][0]['max_area']
+
+        y_plots = np.zeros((400, 200))
+        for i in range(400):
+            x_plot = np.linspace(0, 7300, 200)
+            chain = random.randint(0, len(pos['chain'])-1)
+            draw = random.randint(0, len(pos['draw'])-1)
+
+            adjust = result['results'][0]['adjust']
+            varfactor, zero = get_varfactor(f_fov, 'fov', x_plot, adjust=adjust, pos=pos, chain=chain, draw=draw)
+
+            y_plot = np.exp(varfactor)/np.exp(zero)
+
+            if angle:
+                x_plot = [distance_to_angle(x) for x in x_plot]
+
+            y_plots[i, :] = y_plot
+
+        top_quantile = np.quantile(y_plots, 0.90, axis=0)
+        bottom_quantile = np.quantile(y_plots, 0.10, axis=0)
+        median = np.quantile(y_plots, 0.5, axis=0)
+        ax1.plot(x_plot, top_quantile, color=colors[n], linestyle='--', linewidth=0.5)
+        ax1.plot(x_plot, bottom_quantile, color=colors[n], linestyle='--', linewidth=0.5)
+        plt.plot(x_plot, median, color=colors[n], linewidth=1, linestyle='-')
+
+    if truth is not None and angle:
+        ax2 = ax1.twinx()
+        x = truth['AOI']
+        bg = truth['Background']
+        raw = truth['Lightning']
+        ax2.plot(x, bg, color='black', label="Integrated GLM background brightness", linestyle=':')
+        ax2.plot(x, raw, color='gray', label="Fraction of lightning signal reaching GLM", linestyle=':')
+        ax2.set_ylim(0, 1.1*max(max(bg), max(raw)))
+        ax2.set_ylabel('GLM sensor data')
+
+    if angle:
+        ax1.set_xlabel('Angle of incident light upon sensor (°)')
+    else:
+        ax1.set_xlabel('distance from fov nadir (km)')
+    plt.xlim(0, max(x_plot))
+    ax1.set_ylabel('Normalized bolide flux')
+    plt.title(title)
+
+    handles, labels = plt.gca().get_legend_handles_labels()
+    lines = []
+    lines.append(Line2D([0], [0], label=r'Central 80\% (\texttt{human})', color=colors[0], linestyle='--'))
+    lines.append(Line2D([0], [0], label=r'Median (\texttt{human})', color=colors[0], linestyle='-'))
+    lines.append(Line2D([0], [0], label=r'Central 80\% (\texttt{machine})', color=colors[1], linestyle='--'))
+    lines.append(Line2D([0], [0], label=r'Median (\texttt{machine})', color=colors[1], linestyle='-'))
+    handles.extend(lines)
+
+    plt.legend(handles=handles, frameon=False, loc='lower center', ncol=2)